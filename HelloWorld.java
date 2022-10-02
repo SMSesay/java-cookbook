f4897eae8a99503448cc5c312a1499d990d88764
Sahr Sesay <martin.sesay@hotmail.co.uk>
2022-10-03 00:50:52 +0100

    Add files via upload

---
+++ HelloWorld.java
@@ -0,0 +1,16 @@
+package cookbook;
+
+/**
+ * This class prints out Hello World in the terminal window
+ * 
+ * @author smartin
+ *
+ */
+public class HelloWorld {
+	
+	public static void main(String[] args) {
+		
+		System.out.println("Hello World");
+		
+	}
+}
